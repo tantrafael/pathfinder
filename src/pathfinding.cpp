63ebc2ab19d1b75a9e3b8a04cf6d2a34777603f7
Rafael Hedman <rafael@rafael.se>
2024-11-07 16:28:44 +0100

    Experiment with reference code

--- src/pathfinding.cpp
+++ src/pathfinding.cpp
@@ -3,10 +3,10 @@
 namespace Pathfinding
 {
 	bool FindPath(std::pair<int, int> Start,
-				  std::pair<int, int> Target,
-				  const std::vector<int>& Map,
-				  std::pair<int, int> MapDimensions,
-				  std::vector<int>& OutPath)
+	              std::pair<int, int> Target,
+	              const std::vector<int>& Map,
+	              std::pair<int, int> MapDimensions,
+	              std::vector<int>& OutPath)
 	{
 		return false;
 	}
