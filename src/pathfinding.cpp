576daef80b2bbf65539a6221cfd05ba21210a4fd
Rafael Hedman <rafael@rafael.se>
2024-11-13 10:47:45 +0100

    Add mutexes for priority queues

--- src/pathfinding.cpp
+++ src/pathfinding.cpp
@@ -121,6 +121,7 @@ namespace pathfinding
 
 				if (IsImpassable)
 				{
+					std::lock_guard<std::mutex> lock(OutGrid.ImpassableMutex);
 					const SquareGrid::Location Location{Column, Row};
 					OutGrid.Impassable.insert(Location);
 				}
